73cd8388138d50b967ab4c11cac098900194cade
h-jun01 <jyun.0110@icloud.com>
2020-08-12 01:06:05 +0900

    課題1の実装

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -3,10 +3,30 @@ import UIKit
 final class Question1ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var addTextButton: UIButton!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var label: UILabel!
     
+    // 画面が読み込まれたときに行う処理
     override func viewDidLoad() {
         super.viewDidLoad()
+        textView.text! = ""
+        textField.text! = ""
+        label.isHidden = true
+    }
+    
+    // textFieldに入力された文字列をtextViewに表示
+    @IBAction func addText(_ sender: UIButton) {
+        if textField.text! == "" {
+            label.isHidden = false
+        } else {
+            textView.text! += textField.text! + "\n"
+            textField.text! = ""
+            label.isHidden = true
+        }
+    }
+    
+    // textViewの中身を空にする
+    @IBAction func clearText(_ sender: UIButton) {
+        textView.text! = ""
     }
 }
