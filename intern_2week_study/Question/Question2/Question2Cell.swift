83a0386907add12084f50fef51c3306624456d39
h-jun01 <jyun.0110@icloud.com>
2020-08-13 00:29:16 +0900

    課題2の実装

--- intern_2week_study/Question/Question2/Question2Cell.swift
+++ intern_2week_study/Question/Question2/Question2Cell.swift
@@ -1,6 +1,12 @@
+import Nuke
 import UIKit
 
 class Question2Cell: UITableViewCell {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var areaImageView: UIImageView!
+    
+    func cellDisplay(label: String, url: URL) {
+        self.label?.text = label
+        Nuke.loadImage(with: url, into: areaImageView)
+    }
 }
