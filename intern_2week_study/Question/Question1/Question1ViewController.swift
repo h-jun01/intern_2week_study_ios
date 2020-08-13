714378a4b4eab0b8e8071061d3e34e2d683aa697
h-jun01 <jyun.0110@icloud.com>
2020-08-13 17:17:42 +0900

    fix:空文字判定をguardとisEmptyに修正、強制アンラップの修正

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -9,24 +9,22 @@ final class Question1ViewController: UIViewController {
     // 画面が読み込まれたときに行う処理
     override func viewDidLoad() {
         super.viewDidLoad()
-        textView.text! = ""
-        textField.text! = ""
         label.isHidden = true
     }
     
     // textFieldに入力された文字列をtextViewに表示
     @IBAction func addText(_ sender: UIButton) {
-        if textField.text! == "" {
+        guard let inputValue = textField.text, !inputValue.isEmpty else {
             label.isHidden = false
-        } else {
-            textView.text! += textField.text! + "\n"
-            textField.text! = ""
-            label.isHidden = true
+            return
         }
+        textView.text += inputValue + "\n"
+        textField.text = ""
+        label.isHidden = true
     }
     
     // textViewの中身を空にする
     @IBAction func clearText(_ sender: UIButton) {
-        textView.text! = ""
+        textView.text = ""
     }
 }
