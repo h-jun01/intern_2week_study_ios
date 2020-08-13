2da8e622f97ade8d9ae4e5dc53de468bc86aecaa
h-jun01 <jyun.0110@icloud.com>
2020-08-13 21:53:09 +0900

    fix:指摘箇所の変更

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -4,27 +4,31 @@ final class Question1ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var textView: UITextView!
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var warningLabel: UILabel!
     
     // 画面が読み込まれたときに行う処理
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.isHidden = true
+        warningLabel.isHidden = true
     }
     
     // textFieldに入力された文字列をtextViewに表示
     @IBAction func addText(_ sender: UIButton) {
         guard let inputValue = textField.text, !inputValue.isEmpty else {
-            label.isHidden = false
+            warningLabel.isHidden = false
             return
         }
         textView.text += inputValue + "\n"
         textField.text = ""
-        label.isHidden = true
+        warningLabel.isHidden = true
     }
     
     // textViewの中身を空にする
     @IBAction func clearText(_ sender: UIButton) {
+        clearText()
+    }
+    
+    private func clearText() {
         textView.text = ""
     }
 }
