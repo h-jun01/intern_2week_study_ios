0201f0750445211a3bca444ff3f669843dc62123
h-jun01 <jyun.0110@icloud.com>
2020-08-14 18:59:53 +0900

    課題3-1実装

--- intern_2week_study/Question/Question3/Search/SearchViewController.swift
+++ intern_2week_study/Question/Question3/Search/SearchViewController.swift
@@ -2,7 +2,42 @@ import UIKit
 
 final class SearchViewController: UIViewController {
     
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    @IBAction func searchButtonTapped(_ sender: UIButton) {
+        searchArticles()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    private func searchArticles() {
+        // 多重タップ防止
+        searchButton.isEnabled = false
+        
+        guard let text = textField.text, !text.isEmpty else { return }
+        
+        APIClient.fetchArticles(keyword: text) { [weak self] result in
+            // URLSessionはbackground threadで行われる為UIの更新を明示的にMain Theadで行う
+            DispatchQueue.main.sync {
+                switch result {
+                case .success(let articles):
+                    self?.articleListScreen(articles)
+                    
+                case .failure(let error):
+                    let alert = UIAlertController.createErrorAlert(error)
+                    self?.present(alert, animated: true)
+                    
+                }
+                self?.searchButton.isEnabled = true
+            }
+        }
+    }
+    
+    private func articleListScreen(_ articles: [Article]) {
+        let sampleArticleListViewController = SampleArticleListViewController.makeInstance(articles)
+        navigationController?.pushViewController(sampleArticleListViewController, animated: true)
     }
 }
